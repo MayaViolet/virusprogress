53738d2483ce7ecd0edf2f888f34ee5da5289d54
Maya Kerr <maya@violetmay.net>
2016-01-30 10:43:34 +1100

    Stub GameModel

---
+++ Assets/CoreGame/GameModel.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameModel : MonoBehaviour {
+
+	public enum ActionType
+	{
+		Planning
+	};
+
+	public delegate void ActionCompleteCallback(ActionType completedAction);
+
+	public event ActionCompleteCallback OnActionComplete;
+
+	public void PerformAction(ActionType actionToPerform)
+	{
+		//Do stuff
+		if (OnActionComplete != null)
+		{
+			OnActionComplete(actionToPerform);
+		}
+	}
+}
