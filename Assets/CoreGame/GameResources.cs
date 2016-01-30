3bf06c402ba61b2ffff32aac6787f68cd4c1885a
Maya Kerr <maya@violetmay.net>
2016-01-30 12:59:25 +1100

    Added stub resources class

---
+++ Assets/CoreGame/GameResources.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class GameResources {
+
+	public enum Type
+	{
+		Capacity,
+		Awareness,
+		Willpower
+	};
+
+	public Dictionary<Type, int> contents = new Dictionary<Type, int>();
+
+	public bool Exceeds(GameResources b)
+	{
+		foreach (var key in b.contents.Keys)
+		{
+			if (contents.ContainsKey(key))
+			{
+				if (contents[key] < b.contents[key])
+				{
+					return false;
+				}
+			}
+			else
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public static string[] GetNames()
+	{
+		return System.Enum.GetNames(typeof(Type));
+	}
+}
