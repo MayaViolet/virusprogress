b4e4fd9601a363b8b88ba395908bc445b1ddb745
Ashleigh O'Brien <Ashleigh@Ashleighs-MacBook-Pro.local>
2016-01-31 07:25:34 +1100

    color change scripts

---
+++ Assets/ChangeColor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class ChangeColor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GetComponent<Text> ().DOColor (Color.blue, 2);
+	}
+}
