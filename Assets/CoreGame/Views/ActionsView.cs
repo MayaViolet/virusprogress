f7a636c36c2b64eb7b05e811fe6512f0d3eb8e93
Maya Kerr <maya@violetmay.net>
2016-01-30 13:08:29 +1100

    Stubs for view controllers

---
+++ Assets/CoreGame/Views/ActionsView.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ActionsView : MonoBehaviour {
+
+	public Button buttonPrefab;
+
+	void Start()
+	{
+		
+	}
+
+}
---
+++ Assets/CoreGame/Views/StoryView.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class StoryView : MonoBehaviour {
+
+	public Text textPrefab;
+	public RectTransform content;
+	public int lineHeight = 18;
+
+	void AddLine(string text)
+	{
+		Text newLine = Instantiate(textPrefab) as Text;
+		newLine.text = text;
+		newLine.rectTransform.parent = content;
+		newLine.rectTransform.SetSiblingIndex(0);
+		var size = content.sizeDelta;
+		size.y += lineHeight;
+		content.sizeDelta = size;
+	}
+}
