fdecc08f3211b29f96de54f807acdfa28260faa3
Maya Kerr <maya@violetmay.net>
2016-01-30 11:31:56 +1100

    Stub friend data and first pass sprites

---
+++ Assets/CoreGame/Data/FriendData.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FriendData : ScriptableObject {
+
+	[System.Serializable]
+	public class Friend
+	{
+		public string name;
+	}
+
+	public Friend[] data;
+}
---
+++ Assets/Editor/MonoScriptEditor.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(MonoScript))]
+public class MonoScriptEditor : Editor {
+
+	public override void OnInspectorGUI()
+	{
+		MonoScript targetScript = target as MonoScript;
+		System.Type classType = targetScript.GetClass();
+		if (classType == null)
+		{
+			return;
+		}
+		if (classType.IsSubclassOf(typeof(ScriptableObject)) && !classType.IsSubclassOf(typeof(Editor)))
+		{
+			if (GUILayout.Button("Create Instance"))
+			{
+				ScriptableObject instance = ScriptableObject.CreateInstance(classType);
+				string path = AssetDatabase.GenerateUniqueAssetPath("Assets/"+classType.Name+".asset");
+				AssetDatabase.CreateAsset(instance, path);
+				EditorGUIUtility.PingObject(instance);
+			}
+		}
+	}
+
+}
