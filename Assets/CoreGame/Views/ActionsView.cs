5f0ffb408b5616e54b1b4d421a08de62a05468d5
Maya Kerr <maya@violetmay.net>
2016-01-30 14:05:46 +1100

    Action buttons built from actions enum

--- Assets/CoreGame/Views/ActionsView.cs
+++ Assets/CoreGame/Views/ActionsView.cs
@@ -9,7 +9,24 @@ public class ActionsView : MonoBehaviour {
 
 	void Start()
 	{
-		
+		MakeButtons();
+	}
+
+	void MakeButtons()
+	{
+		foreach (Transform child in transform)
+		{
+			Destroy(child);
+		}
+
+		string[] actions = System.Enum.GetNames(typeof(GameModel.ActionType));
+		foreach (string action in actions)
+		{
+			Button actionButton = Instantiate<Button>(buttonPrefab);
+			actionButton.transform.parent = transform;
+			Text buttonLabel = actionButton.GetComponentInChildren<Text>();
+			buttonLabel.text = action;
+		}
 	}
 
 }
