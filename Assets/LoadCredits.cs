926fa41b618129e5c88cf2871f834ff0f39ee69f
Ashleigh O'Brien <Ashleigh@Ashleighs-MacBook-Pro.local>
2016-01-31 12:53:54 +1100

    added credits, splash,

---
+++ Assets/LoadCredits.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadCredits : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	void loadCredits(){
+		Application.LoadLevel ("Credits"); 
+	}
+}
