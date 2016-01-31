9273c1187831dc38c168391c465ecf1d7255fca2
Maya Kerr <maya@violetmay.net>
2016-01-31 14:43:05 +1100

    Fix virus

--- Assets/CoreGame/Views/VirusView.cs
+++ Assets/CoreGame/Views/VirusView.cs
@@ -3,13 +3,23 @@ using System.Collections;
 
 public class VirusView : MonoBehaviour {
 
+	public GameModel gameModel;
+	public GameObject virusPrefab;
+
 	// Use this for initialization
 	void Start () {
-	
+		gameModel.OnPurchaseUpradeComplete += OnUpgradPurchase;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	void OnUpgradPurchase(UpgradeData.Upgrade upgrade)
+	{
+		if (upgrade == null)
+		{
+			return;
+		}
+		var virus = Instantiate<GameObject>(virusPrefab);
+		virus.transform.SetParent(transform);
+		virus.transform.localPosition = new Vector2(0,0);
+		virus.transform.Translate(Random.insideUnitCircle * 25);
 	}
 }
